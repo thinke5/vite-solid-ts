ec41df2bb543a92f11cd67cf492b5dd3fce1b4ed
xinkewu <xinkewu@tencent.com>
2023-12-26 17:50:13 +0800

    ✨ feat: 增加图片压缩

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import unocss from 'unocss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import imagemin from 'unplugin-imagemin/vite';
 
 export default defineConfig(({ command }) => {
   const isBuild = command === 'build';
   return {
     base: './',
-    plugins: [solid(), unocss(), isBuild && visualizer()],
+    plugins: [solid(), unocss(), isBuild && visualizer(), imagemin()],
   };
 });
